dca34c3284a759630777a9e18a05db6a50e52f1c
Tyler Wright <twright257@gmail.com>
2016-02-08 16:45:32 -0700

    Comments!

--- src/PasswordAttack.java
+++ src/PasswordAttack.java
@@ -8,22 +8,36 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.imageio.IIOException;
 
+/*
+ * Tyler Wright
+ * Feb. 8, 2016
+ * PasswordAttack uses a supplied password dictionary, runs the words from the file through an MD5
+ * hash function and compares the values to those of known hash values, printing the unencrypted word 
+ * as well as the time taken to find it. 
+ */
+
 public class PasswordAttack {
-	private String filePath = "passwords.txt"; 
+	private String filePath = "passwords.txt"; //path to dictionary file
+	//variables of hash values to match on 
+	private String pass0 = "6f047ccaa1ed3e8e05cde1c7ebc7d958"; 
 	private String pass1 = "275a5602cd91a468a0e10c226a03a39c"; 
 	private String pass2 = "b4ba93170358df216e8648734ac2d539"; 
 	private String pass3 = "dc1c6ca00763a1821c5af993e0b6f60a";
 	private String pass4 = "8cd9f1b962128bd3d3ede2f5f101f4fc";
 	private String pass5 = "554532464e066aba23aee72b95f18ba2";
-	private String[] passHash = {pass1, pass2, pass3, pass4, pass5};
+	//hash value array 
+	private String[] passHash = {pass0, pass1, pass2, pass3, pass4, pass5};
+	
 
+	//read through dictionary file and pass words to MD5. Print if match 
 	public void processFile() {
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-		    String line;
-		    long startTime = System.nanoTime(); 
+		    String line;//each dictionary word 
+		    long startTime = System.nanoTime(); //start timer 
 		    while ((line = br.readLine()) != null) {
 		       String md5Value = MD5(line);
 		       for (String p : passHash) {
+		    	   //hash match 
 		    	   if (p.equals(md5Value)) {
 		    		   System.out.println("The Password for Hash Value " + p + " is: " + line);
 		    		   System.out.println("It took " + ((System.nanoTime() - startTime) / 1000000000.0) + " seconds to find the password.\n");
@@ -36,13 +50,15 @@ public class PasswordAttack {
 		}
 	}
 	
+	//convert input string to MD5 hex hash
 	public String MD5(String md5) {
 		try {
+			//very handy way to get MD5 bytes 
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			byte[] inputBytes = md.digest(md5.getBytes());
 			StringBuffer sb = new StringBuffer();
 			for (int i = 0; i < inputBytes.length; ++i) {
-				sb.append(Integer.toHexString((inputBytes[i] & 0xFF) | 0x100).substring(1, 3));
+				sb.append(Integer.toHexString((inputBytes[i] & 0xFF) | 0x100).substring(1, 3)); //pad with zeros and make 2 digit hex
 			}
 			return sb.toString();
 		}catch(NoSuchAlgorithmException e){
@@ -54,6 +70,5 @@ public class PasswordAttack {
 	public static void main(String[] args) {
 		PasswordAttack p = new PasswordAttack(); 
 		p.processFile();
-		//System.out.println(p.MD5("s"));
 	}
 }
